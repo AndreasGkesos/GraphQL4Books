cd5195e572186138ba7ddcc4fd3cf1b3b513d354
Andreas Gkesos <gkesos.andreas@gmail.com>
2019-11-27 01:58:17 +0200

    Add basic mutation and first input type

---
+++ GraphQL4Books/GraphQL/GraphQL4BooksMutation.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL.Types;
+using GraphQL4Books.API.GraphQL.Types;
+using GraphQL4Books.BL;
+using GraphQL4Books.DAL.Repos;
+
+namespace GraphQL4Books.API.GraphQL
+{
+    public class GraphQL4BooksMutation : ObjectGraphType
+    {
+        public GraphQL4BooksMutation(ReviewRepository reviewRepository)
+        {
+            FieldAsync<ReviewType>(
+                "createReview",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<ReviewInputType>> { Name = "review" }
+                ),
+                resolve: async context => {
+                    var review = context.GetArgument<Review>("review");
+                    return await context.TryAsyncResolve(async c => await reviewRepository.AddReviewAsync(review));
+                }
+            );
+        }
+    }
+}
---
+++ GraphQL4Books/GraphQL/Types/ReviewInputType.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL4Books.API.GraphQL.Types
+{
+    public class ReviewInputType : InputObjectGraphType
+    {
+        public ReviewInputType()
+        {
+            Name = "reviewInput";
+            Field<NonNullGraphType<StringGraphType>>("title");
+            Field<StringGraphType>("body");
+            Field<NonNullGraphType<IdGraphType>>("bookId");
+            Field<NonNullGraphType<IdGraphType>>("userId");
+        }
+    }
+}
